d111b37dcc439c5c7ef5c1172623a85b2a9d0595
minhkstn <minhnguyenkstn@gmail.com>
2021-06-30 15:57:28 +0200

    add test video and modify Subjective test UI

--- app/src/main/java/com/athena/mobiledemo/SubjectiveActivity.java
+++ app/src/main/java/com/athena/mobiledemo/SubjectiveActivity.java
@@ -2,7 +2,10 @@ package com.athena.mobiledemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class SubjectiveActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class SubjectiveActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subjective_test);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.test_video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController  = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
     }
 }
