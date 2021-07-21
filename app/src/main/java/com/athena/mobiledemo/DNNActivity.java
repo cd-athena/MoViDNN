e9adb05ee5bed99a29aae975a251b7d06165dbac
Ekrem Çetinkaya <ekrem.cetinkaya@ozu.edu.tr>
2021-07-21 14:23:33 +0200

    Video processing for SR - Reading frames - Saving SR video

--- app/src/main/java/com/athena/mobiledemo/DNNActivity.java
+++ app/src/main/java/com/athena/mobiledemo/DNNActivity.java
@@ -6,7 +6,6 @@ import android.annotation.SuppressLint;
 import android.content.pm.ActivityInfo;
 import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -21,6 +20,15 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.jcodec.api.FrameGrab;
+import org.jcodec.api.JCodecException;
+import org.jcodec.api.PictureWithMetadata;
+import org.jcodec.api.android.AndroidSequenceEncoder;
+import org.jcodec.common.AndroidUtil;
+import org.jcodec.common.io.FileChannelWrapper;
+import org.jcodec.common.io.NIOUtils;
+import org.jcodec.common.model.Picture;
+import org.jcodec.common.model.Rational;
 import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.Interpreter;
 import org.tensorflow.lite.gpu.CompatibilityList;
@@ -40,27 +48,53 @@ import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.SeekableByteChannel;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+class SortedFrame implements Comparable<SortedFrame> {
+    // This is used to store read frames
+    public final double timestamp;
+    public final Bitmap frame;
+
+    public SortedFrame(PictureWithMetadata p) {
+        frame = AndroidUtil.toBitmap(p.getPicture());
+        timestamp = p.getTimestamp();
+    }
+
+    @Override
+    public int compareTo(SortedFrame o2) {
+        return Double.compare(timestamp, o2.timestamp);
+    }
+
+}
 
 public class DNNActivity extends AppCompatActivity {
-
-    Interpreter srModel;
-    String modelName;
+    // Display Objects
     Spinner networkSpinner;
     Button initButton;
     Button startButton;
+    Button videoLoadButton;
     RadioGroup acceleratorPicker;
     RadioButton selectedAccelerator;
+    ImageView display;
+    TextView executionTimeView;
+    // Variables
+    String modelName;
+    Interpreter srModel;
     Interpreter.Options options;
     GpuDelegate gpuDelegate;
     NnApiDelegate nnApiDelegate;
     CompatibilityList compatList;
     Bitmap lrImg;
     String[] availableModels;
+    Boolean isVideoReady = false; // To check if input video is loaded before running SR
     Boolean isDNNReady = false; // To check if the DNN is loaded before running SR
-    ImageView display;
-    TextView executionTimeView;
-
-    final File dir = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/MobileDemo/DNNResults");
+    final File resultsDir = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/MobileDemo/DNNResults");
+    // Video processing variables
+    ArrayList<SortedFrame> videoFrames = new ArrayList<>();
+    ArrayList<Bitmap> srFrames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,13 +104,14 @@ public class DNNActivity extends AppCompatActivity {
         networkSpinner = findViewById(R.id.nnListSpinner);
         initButton = findViewById(R.id.dnnInitButton);
         startButton = findViewById(R.id.dnnStartButton);
+        videoLoadButton = findViewById(R.id.videoLoadButton);
         acceleratorPicker = findViewById(R.id.acceleratorPicker);
         display = findViewById(R.id.srImage);
         executionTimeView = findViewById(R.id.executionTimeView);
         compatList = new CompatibilityList();
 
-        if (!dir.exists()) {
-            if (!dir.mkdir()) {
+        if (!resultsDir.exists()) {
+            if (!resultsDir.mkdir()) {
                 Log.e ("ALERT", "Could not create the directories");
             }
         }
@@ -102,6 +137,7 @@ public class DNNActivity extends AppCompatActivity {
     private void setOnClicks() {
         initButton.setOnClickListener(this::initializeDNN);
         startButton.setOnClickListener(this::runSR);
+        videoLoadButton.setOnClickListener(this::readFrames);
     }
 
     private MappedByteBuffer loadModelFile(String modelName) throws IOException {
@@ -195,20 +231,21 @@ public class DNNActivity extends AppCompatActivity {
                     break;
             }
             isDNNReady = true;
-            // Setup the display in the activity
+            /**
             try {
                 lrImg = BitmapFactory.decodeStream(getAssets().open("frames/0002x4.png"));
                 display.setImageBitmap(lrImg);
             } catch (IOException e) {
                 Log.e("EKREM:", "Error while loading input image: " + e);
             }
+             **/
         }
     }
 
     private void saveImage(Bitmap bmp, String filename) throws IOException {
         // Assume block needs to be inside a Try/Catch block.
         OutputStream fOut = null;
-        File file = new File(dir, filename + ".png"); // the File to save , append increasing numeric counter to prevent files from getting overwritten.
+        File file = new File(resultsDir, filename + ".png"); // the File to save , append increasing numeric counter to prevent files from getting overwritten.
         fOut = new FileOutputStream(file);
 
         bmp.compress(Bitmap.CompressFormat.PNG, 100, fOut); // saving the Bitmap to a file compressed as a JPEG with 85% compression rate
@@ -240,6 +277,54 @@ public class DNNActivity extends AppCompatActivity {
         bmpImage.setPixels(pixels, 0, width, 0, 0, width, height);
 
         return bmpImage;
+    }
+
+    private void saveSrVideo() {
+        FileChannelWrapper out = null;
+        File output = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/MobileDemo/DNNResults/test.mp4");
+
+        try { out = NIOUtils.writableFileChannel(output.getAbsolutePath());
+            AndroidSequenceEncoder encoder = new AndroidSequenceEncoder(out, Rational.R(15, 1));
+            for (Bitmap bitmap : srFrames) {
+                encoder.encodeImage(bitmap);
+            }
+            encoder.finish();
+        } catch (IOException e) {
+            Log.e("EKREM", "Error while encoding video: " + e);
+        } finally {
+            NIOUtils.closeQuietly(out);
+        }
+
+    }
+
+    private void readFrames(View view) {
+        try {
+            Toast.makeText(this, "Reading and processing all frames, this will take some time !", Toast.LENGTH_LONG).show();
+            File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/MobileDemo/DNNResults/video_5_270p.mp4");
+            // File file = new File(String.valueOf(getAssets().openFd("frames/video.mp4")));
+            FrameGrab grab = FrameGrab.createFrameGrab(NIOUtils.readableChannel(file));
+            // For some reason it jitters in the first 3 seconds and dont get any metadata, skip them for now
+            // grab.seekToSecondPrecise(1);
+            for(int i = 0; i < 60; i++) {
+                PictureWithMetadata pic = grab.getNativeFrameWithMetadata();
+                videoFrames.add(new SortedFrame(pic));
+            }
+            // Sort frames based on the times
+            Collections.sort(videoFrames);
+            /**
+            // Reading all frames
+            Picture picture;
+            while (null != (picture = grab.getNativeFrame())) {
+                Bitmap bitmap = AndroidUtil.toBitmap(picture);
+                videoFrames.add(bitmap);
+            }**/
+            // Setup the display in the activity
+            lrImg = videoFrames.get(5).frame;
+            display.setImageBitmap(lrImg);
+            isVideoReady = true;
+        } catch (IOException | JCodecException e) {
+            Log.e("EKREM", "Error while reading frames: " + e);
+        }
     }
 
     private TensorImage prepareInput() {
@@ -275,17 +360,29 @@ public class DNNActivity extends AppCompatActivity {
 
     @SuppressLint("SetTextI18n")
     private void runSR(View view) {
-        if (isDNNReady) {
-            TensorImage lrImage = prepareInput();
-            TensorImage srImage = prepareOutput();
-            long startTime = System.currentTimeMillis();
-            srModel.run(lrImage.getBuffer(), srImage.getBuffer());
-            long difference = System.currentTimeMillis() - startTime;
-            executionTimeView.setText("SR Execution Time: " + difference + "ms - " + 1000/difference + "fps");
-            saveSrImage(srImage);
+        Toast.makeText(this, "Running SR for all frames, this will take some time!", Toast.LENGTH_LONG).show();
+        long difference = 0;
+        if (isDNNReady && isVideoReady) {
+            for(SortedFrame frame : videoFrames) {
+                lrImg = frame.frame;
+                TensorImage lrImage = prepareInput();
+                TensorImage srImage = prepareOutput();
+                long startTime = System.currentTimeMillis();
+                srModel.run(lrImage.getBuffer(), srImage.getBuffer());
+                difference += (System.currentTimeMillis() - startTime);
+                Bitmap srImg = tensorToBitmap(srImage);
+                srFrames.add(srImg);
+            }
+            int numOfFrames = videoFrames.size();
+            difference /= numOfFrames;
+            executionTimeView.setText("Average SR Execution Time: " + difference+ "ms - " + 1000/difference + "fps");
+            // Change the displayed frame
+            display.setImageBitmap(srFrames.get(5));
+            Toast.makeText(this, "Saving SR video, this will take some time!", Toast.LENGTH_LONG).show();
+            saveSrVideo();
         }
         else {
-            Toast.makeText(this, "Please load a model first!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please load a model and video first!", Toast.LENGTH_SHORT).show();
         }
     }
 }
